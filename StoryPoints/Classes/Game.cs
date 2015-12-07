6107889dad6caf79351b0ff5a51bfe31754259d2
gschro <gtschroath@gmail.com>
2015-12-06 21:54:16 -0600

    added Game.cs

---
+++ StoryPoints/Classes/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Concurrent;
+
+
+namespace StoryPoints.Classes
+{
+    public class Game
+    {
+        public ConcurrentDictionary<string, Player> players { get; set; }
+        public string gameId { get; set; }
+        public Integration integration { get; set; }
+
+        public Game()
+        {
+            this.players = new ConcurrentDictionary<string, Player>();
+        }
+    }
+}
