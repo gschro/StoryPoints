ad639b2d138d515b439984882362aa36abe6e37c
Garrett Schroath <gtschroath@gmail.com>
2016-05-10 21:45:21 -0500

    added identity framework

---
+++ StoryPoints/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace StoryPoints.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/jquery").Include(
+                "~/Scripts/Lib/jquery/jquery-{version}.js",
+                "~/Scripts/Lib/jquery/jquery.*",
+                "~/Scripts/Lib/jquery/jquery-ui-{version}.js")
+            );
+
+            bundles.Add(new ScriptBundle("~/Scripts/knockout").Include(
+                 "~/Scripts/Lib/knockout/knockout-{version}.js",
+                 "~/Scripts/Lib/knockout/knockout-deferred-updates.js")
+            );
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                  "~/scripts/jquery.unobtrusive.ajax*",
+                  "~/scripts/jquery.validate.unobtrusive.bootstrap*"));
+        }
+    }
+}
--- StoryPoints/App_Start/Startup.cs
+++ StoryPoints/App_Start/Startup.cs
@@ -3,10 +3,11 @@ using Owin;
 [assembly: OwinStartup(typeof(StoryPoints.Startup))]
 namespace StoryPoints
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
+            ConfigureAuth(app);
             // Any connection or hub wire up and configuration should go here
             app.MapSignalR();
         }
